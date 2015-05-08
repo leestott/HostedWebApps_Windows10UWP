af6dfd961eed810c49a6e8ff432a51ee7fe2c32f
Davide Zordan <info@davidezordan.net>
2015-05-08 13:18:51 +0100

    Added a sample HTML page using JavaScript accessing the Windows Runtime - http://www.davidezordan.net/samples/windows10/hostedwebappssample/page.html

---
+++ SamplePage/script.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+    var element = document.querySelector("#text");    var status = "Sorry about that :( No Windows Runtime here";
+    if (typeof Windows !== "undefined") {
+        if (Windows.Networking.Connectivity.NetworkInformation.getInternetConnectionProfile().getNetworkConnectivityLevel() ==
+            Windows.Networking.Connectivity.NetworkConnectivityLevel.internetAccess) {
+            status = "Happy days! You have access to internet and the Windows Runtime :)";
+        }
+    }
+
+    element.innerHTML = status;
+};
